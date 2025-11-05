d0254a793d1be890d5962f3a9887ae279c6f0754
njosuedev <njosuedev@gmail.com>
2025-11-05 10:00:20 +0200

    var

--- tutorial.ts
+++ tutorial.ts
@@ -3,3 +3,8 @@ console.log("this is a number: " + id);
 
 let name: string = "josoue"
 console.log("my name is: " + name);
+
+let students: string[] = ["josue","murangwa","Chemea","Fabrice"];
+let studentsAge: number[] = [22,21,18,20];
+
+let studentsAds: any[] = ["josue",22,true];
