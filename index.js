eb9894c90f38c32a0a213324942e8f48ac2d9e60
njosuedev <njosuedev@gmail.com>
2025-11-05 10:31:26 +0200

    tsc

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// let id: number = 5;
+// console.log("this is a number: " + id);
+// let name: string = "josoue"
+// console.log("my name is: " + name);
+// let students: string[] = ["josue","murangwa","Chemea","Fabrice"];
+// let studentsAge: number[] = [22,21,18,20];
+// let studentsAds: any[] = ["josue",22,true];
+const conce = (a, b) => {
+    return a + b;
+};
+console.log(conce("heloo ", "Naomi"));
+console.log(conce("10 ", "50"));
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,12 @@
+// let id: number = 5;
+// console.log("this is a number: " + id);
+// let name: string = "josoue"
+// console.log("my name is: " + name);
+// let students: string[] = ["josue","murangwa","Chemea","Fabrice"];
+// let studentsAge: number[] = [22,21,18,20];
+// let studentsAds: any[] = ["josue",22,true];
+const conce = (a: string, b: string) => {
+    return a + b;
+};
+console.log(conce("heloo ", "Naomi"));
+console.log(conce("10 ", "50"));
